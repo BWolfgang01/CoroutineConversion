65d0535eeb0891679e34c5c9a6cf99737907f53c
BWolfgang01 <tun39109@temple.edu>
2024-10-31 11:58:24 -0400

    Removed threads and added coroutine functionality

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.coroutineconversion
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -7,6 +8,12 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
@@ -28,17 +35,25 @@ class MainActivity : AppCompatActivity() {
         true
     })
 
+    val scope = CoroutineScope(Job() + Dispatchers.Default)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
+            scope.launch {
+                revealCake()
             }.start()
+        }
+    }
+
+    private suspend fun revealCake(){
+        repeat(100) {
+            withContext(Dispatchers.Main) {
+                currentTextView.text = ("Current opacity: " + it.toString())
+                cakeImageView.alpha = it / 100f
+            }
+            delay(40)
         }
     }
 }
